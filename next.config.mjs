c1ffb3b5c18bde4e44e94497ae06d57a3d9eb14b
rafael-farcher <work@rafaelfarcher.at>
2024-01-29 13:56:19 +0100

    chore: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
+import nextPWA from "@ducanh2912/next-pwa";
+
+const withPWA = nextPWA({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-};
+});
 
 export default nextConfig;
